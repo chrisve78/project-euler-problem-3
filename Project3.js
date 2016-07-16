443facbbe5704de8b4c76835a22ae2017c1ff40e
Chris Ellis <cellis8200@gmail.com>
2016-07-16 14:50:58 -0700

    Solution

---
+++ Project3.js
@@ -0,0 +1,17 @@
+/*
+The prime factors of 13195 are 5, 7, 13, and 29
+What is the alrgest prime factor of the number 600851475143?
+*/
+
+var maxPrimeFactor = function(number){
+
+  for(var x = 2; x < number; x++){
+
+    while(number % x === 0){
+      number = number / x;
+    }
+  }
+  console.log(number);
+};
+
+maxPrimeFactor(600851475143);
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # project-euler-problem-3
 The Odin Project. Project Euler Problem 3
+
+The Odin Project Link: http://www.theodinproject.com/web-development-101/javascript-basics
+
+Project Euler Link: https://projecteuler.net/problem=3
